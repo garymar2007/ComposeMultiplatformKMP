dc5765dc8d447239f35545ebe20f08061f92d704
garyma <garymar2007@gmail.com>
2025-01-05 21:23:49 +0200

    logo and strings resources.

--- composeApp/src/commonMain/kotlin/com/gary/composemp/App.kt
+++ composeApp/src/commonMain/kotlin/com/gary/composemp/App.kt
@@ -1,13 +1,16 @@
 package com.gary.composemp
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import composemultiplatformhelloworld.composeapp.generated.resources.Res
+import composemultiplatformhelloworld.composeapp.generated.resources.battery_usage_hint
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -16,13 +19,22 @@ fun App(
     batteryManager: MyBatteryManager
 ) {
     MaterialTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "The current battery level is ${batteryManager.getBatteryLevel()}%")
+//        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//            Image(painterResource(Res.drawable.logo), contentDescription = null)
+//        }
+
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                val batteryUsageHint = stringResource( Res.string.battery_usage_hint)
+                Text(
+                    text = "$batteryUsageHint ${batteryManager.getBatteryLevel()}%"
+                )
+            }
         }
+
     }
 }
